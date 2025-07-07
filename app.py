5bf6a861129bacfe3124fe27c25b864f503792e0
Abhineet0904 <neetabhi912@gmail.com>
2025-07-07 12:46:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 API_KEY = st.secrets["YOUTUBE_API_KEY"]
 SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 
-st.set_page_config(page_title="Cyber Mentor Tutorial", layout="wide")
+st.set_page_config(page_title = "Cyber Mentor Tutorial", layout="wide")
 
 st.title("📺 Time pass")
 
@@ -13,40 +13,40 @@ st.title("📺 Time pass")
 query = st.text_input("🔍 Enter search term", "")
 
 # Search button
-if st.button("Search") and query:
+if st.button("Search") and query :
     params = {
-        "part": "snippet",
-        "q": query,
-        "key": API_KEY,
-        "maxResults": 20,
-        "type": "video"
+        "part" : "snippet",
+        "q" : query,
+        "key" : API_KEY,
+        "maxResults" : 20,
+        "type" : "video"
     }
 
-    try:
-        res = requests.get(SEARCH_URL, params=params)
+    try :
+        res = requests.get(SEARCH_URL, params = params)
         res.raise_for_status()
         results = res.json().get("items", [])
         
-        if not results:
+        if not results :
             st.warning("No videos found.")
-        else:
-            for i in range(0, len(results), 3):
+        else :
+            for i in range(0, len(results), 3) :
                 cols = st.columns(3)
-                for j in range(3):
-                    if i + j < len(results):
+                for j in range(3) :
+                    if i + j < len(results) :
                         video = results[i + j]
                         video_id = video["id"]["videoId"]
                         title = video["snippet"]["title"]
                         channel = video["snippet"]["channelTitle"]
                         thumbnail = video["snippet"]["thumbnails"]["medium"]["url"]
 
-                        with cols[j]:
+                        with cols[j] :
                             #st.image(thumbnail, width=320)
                             st.markdown(f"**{title}**")
-                            st.caption(f"Channel: {channel}")
+                            st.caption(f"Channel : {channel}")
                             st.video(f"https://www.youtube.com/watch?v={video_id}")
 
     
-    except requests.exceptions.RequestException as e:
-        st.error("❌ Failed to connect to API")
+    except requests.exceptions.RequestException as e :
+        st.error("Failed to connect to API")
         st.exception(e)
